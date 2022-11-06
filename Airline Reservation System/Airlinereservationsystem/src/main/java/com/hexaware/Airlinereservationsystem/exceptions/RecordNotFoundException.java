faf4027f45b47474a393bd4f2f47498b77c0ac08
Keshav  Tomar <73466362+keshav1771@users.noreply.github.com>
2022-11-06 21:37:21 +0530

    Delete Airline Reservation System/Airlinereservationsystem directory

--- Airline Reservation System/Airlinereservationsystem/src/main/java/com/hexaware/Airlinereservationsystem/exceptions/RecordNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.hexaware.Airlinereservationsystem.exceptions;
-
-public class RecordNotFoundException extends RuntimeException {
-	public RecordNotFoundException(String s) {
-		super(s);
-	}
-
-}
